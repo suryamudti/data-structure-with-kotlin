1df8445b0af8b0e88b539d11edf93096b3a8d051
suryamudti <suryamudti0128@gmail.com>
2021-12-16 11:48:59 +0800

    add graph

---
+++ src/main/kotlin/graph/AdjacencyList.kt
@@ -0,0 +1,76 @@
+package graph
+
+class AdjacencyList<T> : Graph<T> {
+
+    private val adjacencyList: HashMap<Vertex<T>, ArrayList<Edge<T>>> = HashMap()
+
+    override fun createVertex(data: T): Vertex<T> {
+        val vertex = Vertex(adjacencyList.size, data)
+        adjacencyList[vertex] = ArrayList()
+        return vertex
+    }
+
+    override fun addDirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        val edge = Edge(source, destination, weight)
+        adjacencyList[source]?.add(edge)
+    }
+
+    override fun addUnDirectedEdge(source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        addDirectedEdge(source, destination, weight)
+        addDirectedEdge(destination, source, weight)
+    }
+
+    override fun add(edge: EdgeType, source: Vertex<T>, destination: Vertex<T>, weight: Double?) {
+        when (edge) {
+            EdgeType.DIRECTED -> addDirectedEdge(source, destination, weight)
+            EdgeType.UNDIRECTED -> addUnDirectedEdge(source, destination, weight)
+        }
+    }
+
+    override fun edges(source: Vertex<T>): ArrayList<Edge<T>> {
+        return adjacencyList[source] ?: arrayListOf()
+    }
+
+    override fun weight(source: Vertex<T>, destination: Vertex<T>): Double? {
+        return edges(source).firstOrNull {
+            it.destination == destination
+        }?.weight
+    }
+
+    override fun toString(): String {
+        return buildString { // 1
+            adjacencyList.forEach { (vertex, edges) -> // 2
+                val edgeString = edges.joinToString()
+                { it.destination.data.toString() } // 3
+                append("${vertex.data} ---> [ $edgeString ]\n") // 4
+            }
+        }
+    }
+}
+
+fun main() {
+    val graph = AdjacencyList<String>()
+    val singapore = graph.createVertex("Singapore")
+    val tokyo = graph.createVertex("Tokyo")
+    val hongKong = graph.createVertex("Hong Kong")
+    val detroit = graph.createVertex("Detroit")
+    val sanFrancisco = graph.createVertex("San Francisco")
+    val washingtonDC = graph.createVertex("Washington DC")
+    val austinTexas = graph.createVertex("Austin Texas")
+    val seattle = graph.createVertex("Seattle")
+
+    graph.add(EdgeType.UNDIRECTED, singapore, hongKong, 300.0)
+    graph.add(EdgeType.UNDIRECTED, singapore, tokyo, 500.0)
+    graph.add(EdgeType.UNDIRECTED, hongKong, tokyo, 250.0)
+    graph.add(EdgeType.UNDIRECTED, tokyo, detroit, 450.0)
+    graph.add(EdgeType.UNDIRECTED, tokyo, washingtonDC, 300.0)
+    graph.add(EdgeType.UNDIRECTED, hongKong, sanFrancisco, 600.0)
+    graph.add(EdgeType.UNDIRECTED, detroit, austinTexas, 50.0)
+    graph.add(EdgeType.UNDIRECTED, austinTexas, washingtonDC, 292.0)
+    graph.add(EdgeType.UNDIRECTED, sanFrancisco, washingtonDC,
+        337.0)
+    graph.add(EdgeType.UNDIRECTED, washingtonDC, seattle, 277.0)
+    graph.add(EdgeType.UNDIRECTED, sanFrancisco, seattle, 218.0)
+    graph.add(EdgeType.UNDIRECTED, austinTexas, sanFrancisco, 297.0)
+    println(graph)
+}
---
+++ src/main/kotlin/graph/Graph.kt
@@ -0,0 +1,48 @@
+package graph
+
+interface Graph<T> {
+
+    fun createVertex(data: T): Vertex<T>
+
+    fun addDirectedEdge(
+        source: Vertex<T>,
+        destination: Vertex<T>,
+        weight: Double?
+    )
+
+    fun addUnDirectedEdge(
+        source: Vertex<T>,
+        destination: Vertex<T>,
+        weight: Double?
+    )
+
+    fun add(
+        edge: EdgeType,
+        source: Vertex<T>,
+        destination: Vertex<T>,
+        weight: Double?
+    )
+
+    fun edges(source: Vertex<T>): ArrayList<Edge<T>>
+
+    fun weight(
+        source: Vertex<T>,
+        destination: Vertex<T>
+    ): Double?
+
+}
+
+enum class EdgeType {
+    DIRECTED, UNDIRECTED
+}
+
+data class Vertex<T>(
+    val index: Int,
+    val data: T
+)
+
+data class Edge<T>(
+    val source: Vertex<T>,
+    val destination: Vertex<T>,
+    val weight: Double? = null
+)
